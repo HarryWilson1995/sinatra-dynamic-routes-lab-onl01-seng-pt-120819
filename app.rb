f72f01664f0889c8bbf68d8c7911ee5c06f3feee
Harry Wilson <harry.wilson1995a@gmail.com>
2020-03-17 15:15:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,4 +22,10 @@ class App < Sinatra::Base
     @arr.join(' ') + "."
   end
   
+  get '/:operation/:number1/:number2' do 
+    @num1 = params[:number1].to_i 
+    @num2 = params[:number2].to_i 
+    
+  end 
+  
 end
